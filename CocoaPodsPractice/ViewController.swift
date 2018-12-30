80ecea9ef1852da95e5dfe7c96636e0b32f7dee0
iOS Training <iostraining@ganeshs-macbook-pro.local>
2018-12-30 15:14:32 +0530

    Initial Commit

---
+++ CocoaPodsPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoaPodsPractice
+//
+//  Created by iOS Training on 12/30/18.
+//  Copyright © 2018 iOS Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
