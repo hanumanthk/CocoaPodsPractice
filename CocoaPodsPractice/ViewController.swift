8a4fa241b1b8397990fd398d0d2ab2e00e0b26d2
iOS Training <iostraining@ganeshs-macbook-pro.local>
2019-01-02 10:30:53 +0530

    commit

--- CocoaPodsPractice/ViewController.swift
+++ CocoaPodsPractice/ViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import Kingfisher
+
 
 class ViewController: UIViewController {
+    
+    var blueview = UIView(frame: CGRect.init(x: 0, y: 0, width: 160, height: 160))
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        blueview.center = self.view.center
+        blueview.backgroundColor = UIColor.blue
+        
+        
+        let imgview = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 50, height: 50))
+        
+        imgview.center = blueview.center
+        
+        let url = URL.init(string: "https://randomuser.me/api/portraits/men/2.jpg")
+        imgview.kf.setImage(with: url)
+        
+        blueview.addSubview(imgview)
+        self.view.addSubview(blueview)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CocoaPodsPractice' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CocoaPodsPractice
+  pod 'Kingfisher', '~> 5.0'
+  
+
+  target 'CocoaPodsPracticeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CocoaPodsPracticeUITests' do
+    inherit! :search_paths
+    # Pods for testing
+
+  end
+
+end
